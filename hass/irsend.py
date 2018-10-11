4dcc2d2949be20d630c1e5e06bb8368bf3184d75
hj_hejie <hj_hejie@qq.com>
2018-10-11 19:47:31 +0800

    zhgs

--- hass/irsend.py
+++ hass/irsend.py
@@ -2,17 +2,17 @@
 from socket import*
 import json
 
-HOST = 'xxx.xxx.xxx.xxx'
+HOST = '192.168.1.4'
 PORT = 4998
 
 if __name__ == "__main__":
-    with open('irrawcode.json', 'r') as f:
+    with open('irrawcode_ac.json', 'r') as f:
         keys=json.load(f)
     BUFSIZE = 1024
     ADDR = (HOST, PORT)
     tcpCliSock = socket(AF_INET, SOCK_STREAM)
     tcpCliSock.connect(ADDR)
-    tcpCliSock.send(keys['open'].encode())
+    tcpCliSock.send((keys['poweron']+'\\n\\r').encode())
     data = tcpCliSock.recv(BUFSIZE).decode()
     print(data)
     tcpCliSock.close()
--- hass/irserial.py
+++ hass/irserial.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import serial
 from time import sleep
 import json
@@ -5,12 +7,12 @@ import os
 
 if __name__ == '__main__':
     keys={}
-    filename='irrawcode.json'
+    filename='irrawcode_tv.json'
 
     if os.path.exists(filename):
         with open(filename) as f:
             keys=json.load(f)
-    serial = serial.Serial('/dev/ttyUSB1', 115200, timeout=0.5) 
+    serial = serial.Serial('/dev/ttyUSB0', 115200, timeout=0.5) 
     if serial.isOpen() :
         print('open success')
     else :
@@ -22,8 +24,8 @@ if __name__ == '__main__':
             if data != b'':
                 datastr=data.decode()
                 if datastr.startswith('uint16_t rawData'):
-                    print('ir raw code : ', datastr)
                     rawcode=datastr[ datastr.index('{')+1: datastr.index('}')-1 ].replace(' ','')
+                    print('ir raw code : ', rawcode)
                     key=input('input key name>')
                     keys[key]=rawcode
                 else:
