2a2a76ef6da019e0b20a6dd6f80c8147d257148b
hj_hejie <hj_hejie@qq.com>
2018-10-28 22:15:34 +0800

    zh

--- hass/asr.py
+++ hass/asr.py
@@ -1,4 +1,5 @@
 import logging
+import socketserver
 
 from homeassistant.components.light import Light
 
@@ -7,12 +8,24 @@ _LOGGER = logging.getLogger(__name__)
 def setup_platform(hass, config, add_devices, discovery_info=None):
     add_devices([Asr(hass)])
 
+class AsrServer(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        _LOGGER.info('AsrServer handling*******************************')
+        self.request.sendall(b'hejieserver')
+        while True:
+            data=self.request.recv(1)
+            if data.decode().strip():
+                print('hejieserver:'+data.decode())
+
 class Asr(Light):
 
     def __init__(self, hass):
         self._name = 'hejieasr'
         self._state = False
         self.hass=hass
+        server = socketserver.ThreadingTCPServer(('192.168.1.4',8009),AsrServer)
+        server.serve_forever()
 
     @property
     def name(self):
@@ -31,3 +44,7 @@ class Asr(Light):
         print('asr trun off -----------------------------------------------')
         self.hass.services.call('light', 'turn_off', {'entity_id': 'light.hejielight1'})
         self._state=False
+
+if __name__ == '__main__': 
+    server = socketserver.ThreadingTCPServer(('192.168.1.4',8009),AsrServer)
+    server.serve_forever()
