b486f2d544fc126f2c22b07d51e0fba7b9af04fa
hj_hejie <hj_hejie@qq.com>
2018-11-27 16:52:17 +0800

    zhgs

--- arduino/ESP8266Asr/ESP8266Asr.ino
+++ arduino/ESP8266Asr/ESP8266Asr.ino
@@ -16,12 +16,13 @@ void setup()
 void loop()
 {
   if(first){
-    Serial.println("ESP8266Asr sounding***************");
+    Serial.println("ESP8266Asr sounding........");
     for(int i=0;i<20000;i++)
     {
       str_payload += char(analogRead(A0)/4);
     }
     client.print(str_payload);
+    Serial.println("ESP8266Asr sounding ended");
     str_payload="";
     first=false;
   }
