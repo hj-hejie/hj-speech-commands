a06b750bc0f48027d2e71612542572735727691c
hj_hejie <hj_hejie@qq.com>
2018-12-06 09:20:18 +0800

    zhgs

--- hass/asrrecord.py
+++ hass/asrrecord.py
@@ -1,5 +1,6 @@
 import logging
 import time
+import contextlib
 import wave
 import socketserver
 
@@ -19,12 +20,13 @@ class AsrServer(socketserver.BaseRequestHandler):
                 if _bufflen>=20000:
                     wavfile='asr%s.wav'%time.strftime('%Y%m%d%H%M%S', time.localtime())
                     _LOGGER.info(('AsrServer create file %s len %s*************************')%(wavfile, _bufflen))
-                    dest=wave.open(wavfile, 'wb')
-                    dest.setnchannels(1)
-                    dest.setsampwidth(1)
-                    dest.setframerate(10000)
-                    dest.writeframes(_buffer[:20000])
-                    dest.close()
+                    with contextlib.closing(wave.open(wavfile, 'wb')) as wf
+                        bytes16=audioop.lin2lin(_buffer[:20000], 1, 2)
+                        bytes16k=audioop.ratecv(byte16, 2, 1, 10000, 16000, None)[0]
+                        wf.setnchannels(1)
+                        wf.setsampwidth(2)
+                        wf.setframerate(16000)
+                        wf.writeframes(bytes16k)
                     _buffer=_buffer[20000:]
             
 if __name__ == '__main__': 
